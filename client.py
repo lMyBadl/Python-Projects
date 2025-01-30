7fc8fee90fe620ee42caf31e8313e8331f798c51
lMyBadl <yd89782@gmail.com>
2025-01-30 14:25:20 -0600

    suttf

--- client.py
+++ client.py
@@ -1,48 +1,10 @@
 import pygame
 from network import Network
+from player import Player
 
 width = height = 500
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
-
-clientNumber = 0
-
-class Player:
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x, y, width, height)
-        self.vel = 3 
-
-    def draw(self, window):
-        pygame.draw.rect(window, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x,self.y, self.width,self.height)
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
 
 def redrawWindow(window, player, player2):
     window.fill((255, 255, 255))
@@ -54,18 +16,13 @@ def redrawWindow(window, player, player2):
 def main():
     run = True
     n = Network()
-    startPos = read_pos(n.getPos())
-    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
-    p2 = Player(0, 0, 100, 100, (255, 0, 0))
+    p = n.getP()
+
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-
-        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-        p2.update()
+        p2 = n.send(p)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
